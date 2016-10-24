29a2b6016f81831280d19b9f87d2bf1aebecba02
Eric W. Greene <eric@ericwgreene.com>
2016-10-24 16:26:53 -0400

    day 1 code demos

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -1,18 +1,104 @@
-import { Component, Injectable, OnInit, EventEmitter, Input } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+
+@Injectable()
+export class Colors {
+
+	private _lastColors: string[];
+	private _colors: string[] = ['green','saffron','white',
+		'red','blue','yellow','black','brown'];
+
+	getAll(): string[] {
+		if (this._colors !== this._lastColors) {
+			console.log('sort colors executed');
+			this._colors.sort();
+			this._lastColors = this._colors;
+
+		}
+		return this._colors;
+	}
+
+	insert(newColor: string) {
+		this._colors = this._colors.concat(newColor);
+	}		
+
+}
 
 @Component({
 	selector: 'my-app',
-	template: require('./app.component.html'),
-	styles: [require('./app.component.scss')]
+	template: `<div>
+		<h1>{{message | lowercase}}</h1>
+		<div>
+			<label for="color-filter">Filter:</label>
+			<input type="text" id="color-filter" [(ngModel)]="colorFilter">
+		</div>
+		<ul>
+			<li *ngFor="let color of sortedColors">{{color}}</li>
+		</ul>
+		<div>
+			<label for="new-color">New Color:</label>
+			<input type="text" id="new-color" [(ngModel)]="newColor">
+			<button type="button" (click)="addColor()">Add Color</button>
+		</div>
+	</div>`
 })
 export class AppComponent {
 
-	header: string = 'app comp header';
+	constructor(private colorSvc: Colors) { }
 
-	colors: string[] = ['red','blue','yellow','black'];
+	message: string = 'Color List';
+	
+	colorFilter: string = '';
+	newColor: string = '';
+	colors: string[];
+	lastColors: string[];
+	filteredColors: Map<string, string[]>
+		= new Map<string, string[]>();
 
-	send() {
-		console.log('test');
+
+	addColor() {
+		this.colorSvc.insert(this.newColor);
 	}
 
+	get sortedColors() {
+
+		this.colors = this.colorSvc.getAll();
+
+		if (this.colors !== this.lastColors) {
+			this.filteredColors.clear();
+			this.lastColors = this.colors;
+		}
+
+		if (!this.filteredColors.has(this.colorFilter)) {
+			console.log('filter colors executed');
+			this.filteredColors.set(
+				this.colorFilter,
+				this.colorSvc.getAll().filter(color => color.startsWith(this.colorFilter)));
+		}
+
+		return this.filteredColors.get(this.colorFilter);
+	}
 }
+
+// @Component({
+// 	selector: 'my-app',
+// 	template: '<h1>{{message}}</h1>'
+// })
+// export class AppComponent {
+
+// 	message: string = "Hi Class!";
+
+// }
+
+var t = { id : 2 };
+
+t.id = 3;
+
+Object.defineProperty(t, 'id', {
+	get: function() {
+		return this._id;
+	},
+	set: function(value) {
+		this._id = value;
+	}
+})
+
--- src/js/app/app.module.ts
+++ src/js/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import '../../css/styles.scss';
+// import '../../css/styles.scss';
 
-import { AppComponent } from './app.component';
+import { AppComponent, Colors } from './app.component';
 
 @NgModule({
-	imports: [ BrowserModule ],
+	imports: [ BrowserModule, FormsModule ],
 	declarations: [ AppComponent ],
+	providers: [ Colors ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
