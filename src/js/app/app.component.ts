e528b93cf850d4cc88877c6fafe87a5f02ba25c7
Eric W. Greene <eric@ericwgreene.com>
2016-10-27 12:28:20 -0400

    added code demos

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable,
+	OpaqueToken, Inject } from '@angular/core';
+
+interface MyService {
+	doIt(componentName: string) : void;
+}
+
+const MyServiceToken = new OpaqueToken("MyService");
+
+
 
 @Injectable()
 export class SecondService {
@@ -7,45 +16,82 @@ export class SecondService {
 	}
 }
 
+let firstServiceInstanceCount: number = 0;
+
 @Injectable()
-export class FirstService {
+export class FirstService implements MyService {
 
-	constructor(private secondSvc: SecondService) { }
+	localId: number = 0;
 
-	doIt() {
+	constructor(private secondSvc: SecondService) {
+		this.localId = ++firstServiceInstanceCount;
+		console.log('new instance of first service');
+	}
+
+	doIt(componentName: string) {
+		console.log(`first service instance id: ${this.localId}, component: ${componentName}`);
 		console.log('did it!');
 		this.secondSvc.doIt2();
 	}
 }
 
-const altFirstService = {
-	doIt: () => console.log('alternate first service')
-};
-
-const useFirstSvcAlt = false;
-
-const firstServiceFactory = (secondSvc: SecondService) => {
-	if (useFirstSvcAlt) {
-		return altFirstService;
-	} else {
-		return new FirstService(secondSvc);
-	}
-};
-
 @Component({
-	selector: 'my-app',
-	template: '',
-	providers: [{
-		provide: FirstService, useFactory: firstServiceFactory, deps: [ SecondService ]
-	},
-	SecondService ]
+	selector: 'child-demo',
+	template: 'Child Demo<child-demo3></child-demo3>',
+	providers: [ FirstService ]
 })
-export class AppComponent implements OnInit {
+export class ChildDemo implements OnInit {
 
 	constructor(private firstSvc: FirstService) { }
 
 	ngOnInit() {
-		this.firstSvc.doIt();
+		this.firstSvc.doIt('child-demo');
+	}	
+
+}
+
+@Component({
+	selector: 'child-demo2',
+	template: 'Child Demo 2'
+})
+export class ChildDemo2 implements OnInit {
+
+	constructor(private firstSvc: FirstService) { }
+
+	ngOnInit() {
+		this.firstSvc.doIt('child-demo2');
+	}	
+
+}
+
+@Component({
+	selector: 'child-demo3',
+	template: 'Child Demo 3'
+})
+export class ChildDemo3 implements OnInit {
+
+	constructor(private firstSvc: FirstService) { }
+
+	ngOnInit() {
+		this.firstSvc.doIt('child-demo3');
+	}	
+
+}
+
+@Component({
+	selector: 'my-app',
+	//template: 'App<child-demo></child-demo><child-demo2></child-demo2>',
+	template: '',
+	providers: [
+		{ provide: MyServiceToken, useClass: FirstService }
+	]
+})
+export class AppComponent implements OnInit {
+
+	constructor(@Inject(MyServiceToken) private mySvc: MyService) { }
+
+	ngOnInit() {
+		this.mySvc.doIt('app');
 	}
 
 }
--- src/js/app/app.module.ts
+++ src/js/app/app.module.ts
@@ -5,13 +5,14 @@ import { FormsModule } from '@angular/forms';
 import '../../css/styles.scss';
 
 import {
-	AppComponent
+	AppComponent, FirstService, SecondService, ChildDemo, ChildDemo2, ChildDemo3
 } from './app.component';
 
+
 @NgModule({
-	imports: [ BrowserModule,
-		FormsModule ],
-	declarations: [ AppComponent ],
+	imports: [ BrowserModule, FormsModule ],
+	declarations: [ AppComponent, ChildDemo, ChildDemo2, ChildDemo3 ],
+	providers: [ { provide: FirstService, useClass: FirstService }, SecondService ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
