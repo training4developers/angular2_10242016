5fdced570436baf4474737d0f168d36e1aeb2c61
Eric W. Greene <eric@ericwgreene.com>
2016-10-27 10:52:19 -0400

    added code demos

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 
-
-export class FirstService {
-	doIt() {
-		console.log('did it!');
+@Injectable()
+export class SecondService {
+	doIt2() {
+		console.log('did it 2!');
 	}
 }
+
+@Injectable()
+export class FirstService {
+
+	constructor(private secondSvc: SecondService) { }
+
+	doIt() {
+		console.log('did it!');
+		this.secondSvc.doIt2();
+	}
+}
+
+const altFirstService = {
+	doIt: () => console.log('alternate first service')
+};
+
+const useFirstSvcAlt = false;
+
+const firstServiceFactory = (secondSvc: SecondService) => {
+	if (useFirstSvcAlt) {
+		return altFirstService;
+	} else {
+		return new FirstService(secondSvc);
+	}
+};
 
 @Component({
 	selector: 'my-app',
 	template: '',
-	providers: [ FirstService ]
+	providers: [{
+		provide: FirstService, useFactory: firstServiceFactory, deps: [ SecondService ]
+	},
+	SecondService ]
 })
 export class AppComponent implements OnInit {
 
