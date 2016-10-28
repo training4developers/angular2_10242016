1412ec749b12c7894b472f4b9bea90e68bfecd59
Eric W. Greene <eric@ericwgreene.com>
2016-10-27 23:14:35 -0400

    added redis code

--- config/redis.js
+++ config/redis.js
@@ -1,20 +1,79 @@
-var redis = require('redis');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const redis = require('redis');
  
 const redisPort = 16208;
 const redisHost = 'pub-redis-16208.us-east-1-3.7.ec2.redislabs.com';
 const redisPass = 't$dcl@ss!';
- 
-var client = redis.createClient(redisPort, redisHost, {no_ready_check: true});
-client.auth(redisPass, function (err) {
-    if (err) throw err;
+
+const appPort = 3020;
+
+const redisClient = new Promise((resolve, reject) => {
+
+	const client = redis.createClient(redisPort, redisHost, { no_ready_check: true });
+
+	client.auth(redisPass, err => {
+		if (err) reject(err);
+	});
+
+	client.on('connect', err => {
+		if (err) reject(err);
+		resolve(client);
+	});
+
 });
- 
-client.on('connect', function() {
-    console.log('Connected to Redis');
- 
-    client.set("foo", "bar", redis.print);
-    client.get("foo", function (err, reply) {
-            if (err) throw err;
-            console.log(reply.toString());
-    });
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/:key', (req, res) => {
+
+	redisClient.then(client => {
+		client.get(req.params.key, (err, reply) => {
+
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+
+			if (reply === null) {
+				res.sendStatus(404);
+			} else {
+				res.set('Content-Type', 'application/json');
+				res.send(reply);
+			}
+			
+		});
+	});
+
+});
+
+app.post('/:key', (req, res) => {
+
+	redisClient.then(client => {
+
+		const value = JSON.stringify(req.body);
+
+		client.set(req.params.key, value, (err, reply) => {
+
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+
+			client.publish('update', JSON.stringify({
+				key: req.params.key,
+				value
+			}));
+
+			res.json(reply);
+		});
+	});
+});
+
+
+app.listen(appPort, () => {
+	console.log(`redis web server listening on port ${appPort} `)
 });
--- config/web-sockets.js
+++ config/web-sockets.js
@@ -1,20 +1,54 @@
-const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({ port: 3020 });
+const ws = require('ws');
+const redis = require('redis');
+ 
+const wsPort = 3030;
+
+const redisPort = 16208;
+const redisHost = 'pub-redis-16208.us-east-1-3.7.ec2.redislabs.com';
+const redisPass = 't$dcl@ss!';
+
+const redisClient = new Promise((resolve, reject) => {
+
+	const client = redis.createClient(redisPort, redisHost, { no_ready_check: true });
+
+	client.auth(redisPass, err => {
+		if (err) reject(err);
+	});
+
+	client.on('connect', err => {
+		if (err) reject(err);
+		resolve(client);
+	});
+
+});
+
+const wss = new ws.Server({ port: wsPort }, () => {
+	console.log(`web socket server started on port ${wsPort}`);
+});
  
 wss.on('connection', ws => {
 
-	console.log('connection opened');
+	console.log(`web socket connection opened on port ${wsPort}`);
 
-	let counter = 0;
-	let handle;
+	// let counter = 0;
+	// let handle;
 
 	ws.on('close', () => {
-		console.log('connection closed');
-		clearInterval(handle);
+		console.log(`web socket connection closed on port ${wsPort}`);
+		//clearInterval(handle);
 	});
 
-	handle = setInterval(() => {
-		ws.send(JSON.stringify(counter++));
-	}, 500);
+	// handle = setInterval(() => {
+	// 	ws.send(JSON.stringify(counter++));
+	// }, 500);
+
+	redisClient.then(client => {
+		client.on('message', (change, message) => {
+			console.log(message);
+			ws.send(message);
+		});
+		client.subscribe('update');
+	});
+
  
 });
